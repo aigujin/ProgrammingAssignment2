7b5a95855de954e7207a33ae0389631b2832472a
aigujin <aiguzhinov@gmail.com>
2014-04-24 16:40:28 +0100

    Revert "CacheInverse"
    This reverts commit 9321ef5e3b76e2f8ec6faf2e3510107fe14bb1a7.

--- cachematrix.R
+++ cachematrix.R
@@ -2,33 +2,14 @@
 ## functions do
 
 ## Write a short comment describing this function
-###makeCacheMatrix  saves data  in a new environment
+
 makeCacheMatrix <- function(x = matrix()) {
-        set <- function(y) {
-                x <<- y
-                m <<- NULL
-        }
-        get <- function() x
-        setinverse <- function(solve) m <<- solve
-        getinverse <- function() m
-        list(set = set, get = get,
-                setinverse = setinverse,
-                getinverse = getinverse)
+
 }
 
 
 ## Write a short comment describing this function
-#On the first run, the cacheSolve calulates the inverse of matrix and stores the result in the environment created by the makeCacheMatrix. On the subsequient runs of the same matrix, the funtions checks if the inverse had been already calculated. If it did, the function returns the inverse. 
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
-        m <- x$getinverse()
-        if(!is.null(m)) {
-                message("getting cached data")
-                return(m)
-        }
-        data <- x$get()
-        m <- solve(data, ...)
-        x$setinverse(m)
-        m
 }
